89a6e0a8937df50387f00da817de9bab67c2ac84
Jeanne Bisson <jeanne.bisson@insa-rennes.fr>
2025-03-07 16:19:12 +0100

    TP6 fini + commentaires

--- app/src/main/java/com/insa/mygamelist/MainActivity.kt
+++ app/src/main/java/com/insa/mygamelist/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.insa.mygamelist
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -27,7 +25,6 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -37,13 +34,13 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -66,10 +63,8 @@ import coil3.compose.AsyncImage
 import com.insa.mygamelist.data.Game
 import com.insa.mygamelist.data.IGDB
 import com.insa.mygamelist.data.Platform
-import com.insa.mygamelist.data.Platform_Logo
 import com.insa.mygamelist.ui.theme.MyGamesListTheme
 import kotlinx.serialization.Serializable
-import okhttp3.internal.notifyAll
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
@@ -79,7 +74,6 @@ object HomeRoute
 @Serializable
 data class GameRoute(val id : Long)
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -87,16 +81,17 @@ data class GameRoute(val id : Long)
 
         enableEdgeToEdge()
         setContent {
-            val navController = rememberNavController()
+            val navController = rememberNavController()                         // Permet de naviguer entre les deux pages de l'appli
+            val favoriteGames = remember { mutableStateOf(setOf<Long>()) }      // Permet de garder les mêmes favoris entre les deux pages de l'appli
 
             MyGamesListTheme {
                 NavHost(navController, startDestination = HomeRoute) {
                     composable<HomeRoute> {
-                        HomeScreen(navController)
+                        HomeScreen(navController, favoriteGames)
                     }
                     composable<GameRoute> { backStackEntry ->
                         val route = backStackEntry.toRoute<GameRoute>()
-                        GameScreen(route.id,navController)
+                        GameScreen(route.id,navController, favoriteGames)
                     }
                 }
             }
@@ -108,17 +103,17 @@ data class GameRoute(val id : Long)
     @Composable
     fun LogoDisplay(platform : Platform){
         AsyncImage(
-            model = "https:" + IGDB.platform_logos.find({ platform.platform_logo == it.id })?.url,
+            model = "https:" + IGDB.platform_logos.find{ platform.platform_logo == it.id }?.url,       // On récupère l'image
             contentDescription = "Logos of the platforms",
             contentScale = ContentScale.Crop,
             modifier = Modifier.size(90.dp).padding(10.dp)
         )
     }
 
-    // Permet d'afficher la case d'un jeu sur le HomeScreen
+    // Permet de créer la case d'un jeu sur le HomeScreen
     @Composable
-    fun GameCard(game : Game, navController: NavController){
-        var isFavorite by rememberSaveable { mutableStateOf(false) }
+    fun GameCard(game: Game, navController: NavController, favoriteGames: MutableState<Set<Long>>){
+        val isFavorite = favoriteGames.value.contains(game.id)      // On vérifie si le jeu est enregistré en tant que favori
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
@@ -126,24 +121,24 @@ data class GameRoute(val id : Long)
                 .height(100.dp)
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(16.dp))
-                .clickable{
-                    navController.navigate(GameRoute(game.id))
-                }
+                .clickable{navController.navigate(GameRoute(game.id))}      // Permet de passer sur le GameScreen du jeu
                 .background(Color(229, 224, 232)),
         ) {
+            // Affichage de l'image à gauche
             AsyncImage(
-                model = "https:" + IGDB.covers.find({ game.cover == it.id })?.url,      // on récupère l'image
+                model = "https:" + IGDB.covers.find{ game.cover == it.id }?.url,      // On récupère l'image
                 contentDescription = "Cover of the game" + game.name,
                 modifier = Modifier.size(90.dp).padding(10.dp)
             )
             Column(modifier = Modifier.width(250.dp).align(Alignment.CenterVertically)) {
+                // Affichage du nome du jeu au milieu
                 Text(
-                    game.name,          // on affiche le nom du jeu
+                    game.name,
                     fontSize = 17.sp,
                     fontWeight = FontWeight.W600,
                     style = TextStyle(textDecoration = TextDecoration.Underline)
                 )
-
+                // Affichage des genres du jeu en-dessous de son nom
                 val mygenres = "Genres : " + IGDB.genres.filter { it.id in game.genres }.joinToString(", ") { it.name }  // on récupère tous les genres associé au jeu et on les sépare avec une ,
                 Text(
                     mygenres,                               // affiche les genres
@@ -151,11 +146,18 @@ data class GameRoute(val id : Long)
                     overflow = TextOverflow.Ellipsis        // permet de mettre les ... quand la liste de genres est trop longue
                 )
             }
-            Column(){
-                IconButton(onClick = { isFavorite = !isFavorite }) {
+            Column{
+                // Affichage du bouton "favori" à droite
+                IconButton(onClick = {
+                    favoriteGames.value = if (isFavorite) {
+                        favoriteGames.value - game.id
+                    } else {
+                        favoriteGames.value + game.id
+                    }
+                }) {
                     Icon(
-                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,  // en fonction de la valeur de isSearchVisible, l'icone sera une loupe ou une croix
-                        contentDescription = "Afficher/Cacher la recherche"
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,  // en fonction de la valeur de isFavorite, l'icône sera un coeur rempli ou non
+                        contentDescription = "Favori"
                     )
                 }
             }
@@ -164,11 +166,11 @@ data class GameRoute(val id : Long)
 
     // Ecran d'affichage d'accueil, qui contient la liste de tous les jeux
     @Composable
-    fun HomeScreen(navController: NavHostController) {
-        var isSearchVisible by rememberSaveable { mutableStateOf(false) }
-        var searchText by rememberSaveable { mutableStateOf("") }
+    fun HomeScreen(navController: NavHostController, favoriteGames: MutableState<Set<Long>>) {
+        var isSearchVisible by rememberSaveable { mutableStateOf(false) }       // Permet de savoir si l'icône de recherche est activée ou non
+        var searchText by rememberSaveable { mutableStateOf("") }               // Contiendra le texte de recherche rentré par l'utilisateur
 
-        // Filtrer la liste des jeux en fonction du texte recherché
+        // Filtre la liste des jeux en fonction du texte recherché
         val filteredGames = IGDB.games.filter { game ->
             game.name.contains(searchText, ignoreCase = true) ||
                     IGDB.genres
@@ -187,7 +189,7 @@ data class GameRoute(val id : Long)
                 actions = {
                     IconButton(onClick = { isSearchVisible = !isSearchVisible }) {
                         Icon(
-                            imageVector = if (isSearchVisible) Icons.Default.Close else Icons.Default.Search,  // en fonction de la valeur de isSearchVisible, l'icone sera une loupe ou une croix
+                            imageVector = if (isSearchVisible) Icons.Default.Close else Icons.Default.Search,  // en fonction de la valeur de isSearchVisible, l'icône sera une loupe ou une croix
                             contentDescription = "Afficher/Cacher la recherche"
                         )
                     }
@@ -207,7 +209,7 @@ data class GameRoute(val id : Long)
             if(isSearchVisible) {
                 if (filteredGames.isEmpty()) {
                     Text(
-                        "No match found :(",
+                        text = "No match found :(",
                         modifier = Modifier.padding(innerPadding).offset(x = 10.dp, y = 70.dp),
                         fontStyle = FontStyle.Italic
                     )
@@ -218,12 +220,9 @@ data class GameRoute(val id : Long)
                             .offset(y = if (isSearchVisible) 60.dp else 0.dp)
                     ) {
                         items(
-                            items = filteredGames,
-                            itemContent = {                 //va itérer sur chaque jeu correspondant à la recherche
-                                    game ->
-                                //val game = IGDB.games[index]        // on récupère le jeu
-                                GameCard(game, navController)       // on crée la carte du jeu
-                            })
+                            items = filteredGames,            // Va itérer sur chaque jeu correspondant à la recherche
+                            itemContent = {game -> GameCard(game, navController, favoriteGames)}      // On crée la carte du jeu
+                        )
                     }
                 }
             }else{
@@ -233,12 +232,9 @@ data class GameRoute(val id : Long)
                         .offset(y = if (isSearchVisible) 60.dp else 0.dp)
                 ) {
                     items(
-                        items = IGDB.games,
-                        itemContent = {                 //va itérer sur chaque jeu correspondant à la recherche
-                                game ->
-                            //val game = IGDB.games[index]        // on récupère le jeu
-                            GameCard(game, navController)       // on crée la carte du jeu
-                        })
+                        items = IGDB.games,         // Va itérer sur chaque jeu correspondant à la recherche
+                        itemContent = { game -> GameCard(game, navController, favoriteGames)}       // On crée la carte du jeu
+                    )
                 }
             }
         }
@@ -246,8 +242,8 @@ data class GameRoute(val id : Long)
 
     // Ecran d'affichage des détails d'un jeu
     @Composable
-    fun GameScreen(id : Long, navController: NavHostController) {
-        val game = IGDB.games.find({ id == it.id })
+    fun GameScreen(id : Long, navController: NavHostController, favoriteGames: MutableState<Set<Long>>) {
+        val game = IGDB.games.find{ id == it.id }
         val name = game?.name ?: "Unfound"          //Obligé de mettre un Elvis car Text ne prend pas de type String? (type renvoyé par .find)
         val genres = IGDB.genres.filter { it.id in (game?.genres ?: listOf(String)) }.joinToString(", ") { it.name }  // on récupère tous les genres associé au jeu et on les sépare avec une ,
         val summary = game?.summary ?: "Unfound"
@@ -258,17 +254,33 @@ data class GameRoute(val id : Long)
                 containerColor = Color(144,238,144),
                 titleContentColor = Color.Black,
             ), title = { Text(name) },
-                navigationIcon = { IconButton(onClick = {navController.navigateUp()}){      // flèche de retour arrière
+                navigationIcon = { IconButton(onClick = {navController.navigateUp()}){      // Flèche de retour arrière
                     Icon(
                         imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = "Localized description"
                     )
-                }})
+                }},
+                actions = {
+                    val isFavorite = favoriteGames.value.contains(id)
+                    IconButton(onClick = {
+                        favoriteGames.value = if (isFavorite) {     // Si le jeu est en favori et qu'on clique sur l'icône
+                            favoriteGames.value - id                // Alors le jeu n'est plus en favori
+                        } else {
+                            favoriteGames.value + id                // Sinon il est ajouté aux favoris
+                        }
+                    }) {
+                        Icon(
+                            imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,         // Si le jeu est en favori, l'icône favori est plein, sinon il est vide
+                            contentDescription = "Favori"
+                        )
+                    }
+                }
+            )
         }, modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Column (horizontalAlignment = Alignment.CenterHorizontally,
-                modifier=Modifier.padding(innerPadding)){
+            Column (horizontalAlignment = Alignment.CenterHorizontally, modifier=Modifier.padding(innerPadding)){
+                // Affichage du nom du jeu en haut de la page
                 Text(
-                    name,
+                    text = name,
                     textAlign = TextAlign.Center,
                     modifier = Modifier.fillMaxWidth().padding(10.dp),
                     fontSize = 17.sp,
@@ -276,31 +288,35 @@ data class GameRoute(val id : Long)
                     style = TextStyle(textDecoration = TextDecoration.Underline)
                 )
 
+                // Affichage de l'image du jeu en dessous
                 AsyncImage(
-                    model = "https:" + IGDB.covers.find({ game?.cover == it.id })?.url,
-                    contentDescription = "Cover of the game" + name,
+                    model = "https:" + IGDB.covers.find{ game?.cover == it.id }?.url,
+                    contentDescription = "Cover of the game $name",
                     modifier = Modifier.size(250.dp).padding(10.dp)
                 )
 
+                // Affichage des genres du jeu en dessous
                 Text(
-                    genres,                               // affiche les genres
+                    text = genres,
                     textAlign = TextAlign.Center,
                     fontSize = 13.sp,
                     fontStyle = FontStyle.Italic,
-                    maxLines = 1,                           // permet de mettre les genres sur une seule ligne
-                    overflow = TextOverflow.Ellipsis        // permet de mettre les ... quand la liste de genres est trop longue
+                    maxLines = 1,                           // Permet de mettre les genres sur une seule ligne
+                    overflow = TextOverflow.Ellipsis        // Permet de mettre les ... quand la liste de genres est trop longue
                 )
 
+                // Affichage des logos des plateformes sur lesquelles le jeu est diponible en dessous
                 LazyRow {
-                    items(platforms.size) {         //on va itérer sur chaque plateforme associée au jeu
+                    items(platforms.size) {         // On va itérer sur chaque plateforme associée au jeu
                             index ->
                         val platform = platforms[index]
-                        LogoDisplay(platform)       // affichage du logo du jeu
+                        LogoDisplay(platform)       // Affichage du logo du jeu
                     }
                 }
 
+                // Affichage du résumé du jeu en dessous
                 Text(
-                    summary,
+                    text = summary,
                     modifier = Modifier.padding(10.dp)
                 )
             }
